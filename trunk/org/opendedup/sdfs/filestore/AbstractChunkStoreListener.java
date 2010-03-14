838d703b265e35737e249184ad3aa3a4941fb1f3
sam.silverberg <sam.silverberg@856e77c2-2ed6-11df-8e64-fd3be38ee323>
2010-03-14 02:13:21 +0000

    asf

--- trunk/org/opendedup/sdfs/filestore/AbstractChunkStoreListener.java
+++
@@ -1,6 +0,0 @@
-package org.opendedup.sdfs.filestore;
-
-public interface AbstractChunkStoreListener {
-	public abstract void chunkMovedEvent(ChunkEvent e);
-	public abstract void chunkRemovedEvent(ChunkEvent e);
-}
--- trunk/org/opendedup/sdfs/servers/HashEngine.java
+++
@@ -1,5 +0,0 @@
-package org.opendedup.sdfs.servers;
-
-public class HashEngine {
-
-}
