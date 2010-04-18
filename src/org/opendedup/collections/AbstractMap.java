0c1bdcea0a270511ab3b084fdc2356cc18e5ee6f
sam.silverberg <sam.silverberg@856e77c2-2ed6-11df-8e64-fd3be38ee323>
2010-04-18 21:00:11 +0000

    Updates for 0.9 branch

--- src/org/opendedup/collections/AbstractMap.java
+++ src/org/opendedup/collections/AbstractMap.java
@@ -6,12 +6,6 @@ public interface AbstractMap {
 
 	public abstract boolean isClosed();
 
-	public abstract void put(long pos, byte[] data) throws IOException;
-
-	public abstract void remove(long pos) throws IOException;
-
-	public abstract byte[] get(long pos) throws IOException;
-
 	public abstract void sync() throws IOException;
 
 	public abstract void vanish() throws IOException;
