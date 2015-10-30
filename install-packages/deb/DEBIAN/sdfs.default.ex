95a7a77e870335c5f9f2c0b1ae1cf8507a88a2b8
Sam Silverberg <sam.silverberg@gmail.com>
2015-10-29 23:29:10 -0700

    added debian package source

---
+++ install-packages/deb/DEBIAN/sdfs.default.ex
@@ -0,0 +1,10 @@
+# Defaults for sdfs initscript
+# sourced by /etc/init.d/sdfs
+# installed at /etc/default/sdfs by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ install-packages/deb/usr/share/sdfs/odvol.make
@@ -0,0 +1,2 @@
+java  -Dfile.encoding=UTF-8 -classpath /usr/share/sdfs/lib/sdfs.jar:/usr/share/sdfs/lib/* org.opendedup.sdfs.VolumeConfigWriter "$@" --io-safe-sync=true
+
