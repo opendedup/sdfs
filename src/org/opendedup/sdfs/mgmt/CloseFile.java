1df3654c79ab739622b0d47a2f21a69e1725abb7
Sam Silverberg <sam.silverberg@gmail.com>
2015-10-29 22:35:33 -0700

    Added close file

---
+++ src/org/opendedup/sdfs/mgmt/CloseFile.java
@@ -0,0 +1,41 @@
+package org.opendedup.sdfs.mgmt;
+
+import java.io.File;
+
+import java.io.IOException;
+
+
+
+
+
+import org.opendedup.logging.SDFSLogger;
+import org.opendedup.sdfs.Main;
+import org.opendedup.sdfs.filestore.MetaFileStore;
+import org.opendedup.sdfs.io.MetaDataDedupFile;
+import org.opendedup.util.XMLUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class CloseFile {
+	
+	
+	public Element getResult(String cmd, String file) throws IOException {
+		try {
+			Document doc = XMLUtils.getXMLDoc("close-file");
+			Element root = doc.getDocumentElement();
+			File f = new File(Main.volume.getPath() + File.separator+file);
+			if(f.exists()) {
+			MetaDataDedupFile mf = MetaFileStore.getMF(f);
+			mf.getDedupFile(true).forceClose();
+			}
+			return (Element) root.cloneNode(true);
+		} catch (Exception e) {
+			SDFSLogger.getLog().error(
+					"unable to fulfill request on file " + file, e);
+			throw new IOException("request to fetch attributes failed because "
+					+ e.toString());
+		}
+	}
+
+}
+
