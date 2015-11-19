01fa1754c13ea02d31f329dca15b2880144daf18
Sam Silverberg <sam.silverberg@gmail.com>
2015-11-19 04:39:13 -0800

    cloud storage updates

---
+++ src/org/opendedup/sdfs/filestore/cloud/AbstractDownloadListener.java
@@ -0,0 +1,41 @@
+package org.opendedup.sdfs.filestore.cloud;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.opendedup.collections.DataArchivedException;
+
+public abstract class AbstractDownloadListener {
+	AtomicInteger dn = new AtomicInteger(0);
+	AtomicInteger exdn = new AtomicInteger(0);
+	private DataArchivedException dar = null;
+
+	public abstract void commandException(Exception e);
+
+	public abstract void commandResponse(DownloadShard shard);
+
+	public int incrementandGetDN() {
+		return dn.incrementAndGet();
+	}
+
+	public int getDN() {
+		return dn.get();
+	}
+
+	public int incrementAndGetDNEX() {
+		return exdn.incrementAndGet();
+	}
+
+	public int getDNEX() {
+		return exdn.get();
+	}
+	
+	public synchronized void setDAR(DataArchivedException dar) {
+		if(this.dar == null)
+			this.dar = dar;
+	}
+	
+	public synchronized DataArchivedException getDAR() {
+		return this.dar;
+	}
+
+}
