e56a32e1e80aea7c034449e4f79b274e21c65e7d
Sam Silverberg <sam.silverberg@gmail.com>
2015-11-28 08:47:58 -0800

    fixed readme no being copied over and lastest sdfs.jar

--- install-packages/build.sh
+++ install-packages/build.sh
@@ -6,7 +6,8 @@ sudo rm deb/usr/share/sdfs/bin/libjavafs.so
 sudo cp DEBIAN/libfuse.so.2 deb/usr/share/sdfs/bin/
 sudo cp DEBIAN/libulockmgr.so.1 deb/usr/share/sdfs/bin/
 sudo cp DEBIAN/libjavafs.so deb/usr/share/sdfs/bin/
-sudo chown -R root:root ./deb
+sudo cp ../src/readme.txt deb/usr/share/sdfs/
+sudo chown -R root:root ./deb:
 sudo fpm -s dir -t deb -n sdfs -v 3.0.1 -C deb/ -d fuse --url http://www.opendedup.org -d libxml2 -m sam.silverberg@gmail.com --vendor datishsystems --description "SDFS is an inline deduplication based filesystem"
 sudo rm deb/usr/share/sdfs/bin/libfuse.so.2
 sudo rm deb/usr/share/sdfs/bin/libulockmgr.so.1
