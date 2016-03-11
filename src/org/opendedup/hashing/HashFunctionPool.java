ddb8ee1ed9179e134507b151fa94ad84cb1c1983
Sam Silverberg <sam.silverberg@gmail.com>
2016-03-10 20:24:56 -0800

    merged updates

--- src/org/opendedup/hashing/HashFunctionPool.java
+++ src/org/opendedup/hashing/HashFunctionPool.java
@@ -29,7 +29,7 @@ public class HashFunctionPool {
 			hashLength = Murmur3HashEngine.getHashLenth();
 		} else if (Main.hashType.equalsIgnoreCase(VARIABLE_MURMUR3)) {
 			hashLength = VariableHashEngine.getHashLenth();
-			Main.MAPVERSION = 2;
+			Main.MAPVERSION = 3;
 			max_hash_cluster = VariableHashEngine.getMaxCluster();
 		}
 		SDFSLogger.getLog().info("Set hashtype to " + Main.hashType);
