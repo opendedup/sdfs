f01edd93f5fc12a4f5fb94915c204130ad9de6df
sam.silverberg@gmail.com <sam.silverberg@gmail.com>
2020-07-21 07:31:09 -0700

    Fixed timeout

--- src/org/opendedup/sdfs/mgmt/cli/ProcessShutdown.java
+++ src/org/opendedup/sdfs/mgmt/cli/ProcessShutdown.java
@@ -10,22 +10,25 @@ import org.opendedup.grpc.VolumeServiceGrpc;
 import org.opendedup.grpc.errorCodes;
 import org.opendedup.grpc.VolumeServiceGrpc.VolumeServiceBlockingStub;
 
-import io.grpc.Status;
+
+import io.grpc.Status.Code;
 
 public class ProcessShutdown {
 	public static void runCmd() {
 		VolumeServiceBlockingStub volumeStub = VolumeServiceGrpc.newBlockingStub(MgmtServerConnection.channel);
-		ShutdownRequest req = ShutdownRequest.newBuilder().build();
+		
 		try {
+			ShutdownRequest req = ShutdownRequest.newBuilder().build();
 			ShutdownResponse resp = volumeStub.withDeadlineAfter(5,TimeUnit.SECONDS).shutdownVolume(req);
 			if(resp.getErrorCode() != errorCodes.NOERR) {
 				System.out.println(resp.getError());
 				System.exit(1);
 			}
 		}catch(io.grpc.StatusRuntimeException e) {
-			if(e.getStatus()== Status.DEADLINE_EXCEEDED) {
+			if(e.getStatus().getCode() == Code.DEADLINE_EXCEEDED) {
 				return;
 			} else {
+				System.out.println(e.getStatus());
 				e.printStackTrace();
 				System.exit(0);
 			}
