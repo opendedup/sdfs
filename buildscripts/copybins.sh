8c8048f1600128dd63f366e85aa0d5c86c2650bb
sam.silverberg@gmail.com <sam.silverberg@gmail.com>
2021-04-01 09:07:40 -0700

    added variables

--- buildscripts/copybins.sh
+++ buildscripts/copybins.sh
@@ -1,15 +1,15 @@
-gsutil cp gs://${_DIST_BUCKET}/sdfscli/sdfscli-$BRANCH_NAME /tmp/ 
+gsutil cp gs://$1/sdfscli/sdfscli-$2 /tmp/ 
 rm -rf /workspace/install-packages/deb/sbin/sdfscli
 rm -rf /workspace/install-packages/deb/usr/share/sdfs/sdfscli
-cp /tmp/sdfscli-$BRANCH_NAME  /workspace/install-packages/deb/sbin/sdfscli 
-cp /tmp/sdfscli-$BRANCH_NAME  /workspace/install-packages/deb/usr/share/sdfs/sdfscli
+cp /tmp/sdfscli-$2  /workspace/install-packages/deb/sbin/sdfscli 
+cp /tmp/sdfscli-$2  /workspace/install-packages/deb/usr/share/sdfs/sdfscli
 md5sum /workspace/install-packages/deb/sbin/sdfscli
-md5sum /tmp/sdfscli-$BRANCH_NAME
-rm /tmp/sdfscli-$BRANCH_NAME
-gsutil cp gs://${_DIST_BUCKET}/hybrics-fs/mount.sdfs-$BRANCH_NAME /tmp/ 
+md5sum /tmp/sdfscli-$2
+rm /tmp/sdfscli-$2
+gsutil cp gs://$1/hybrics-fs/mount.sdfs-$2 /tmp/ 
 rm -rf /workspace/install-packages/deb/sbin/mount.sdfs
 rm -rf /workspace/install-packages/deb/usr/share/sdfs/mount.sdfs
-cp /tmp/mount.sdfs-$BRANCH_NAME  /workspace/install-packages/deb/sbin/mount.sdfs 
-cp /tmp/mount.sdfs-$BRANCH_NAME  /workspace/install-packages/deb/usr/share/sdfs/mount.sdfs
+cp /tmp/mount.sdfs-$2  /workspace/install-packages/deb/sbin/mount.sdfs 
+cp /tmp/mount.sdfs-$2  /workspace/install-packages/deb/usr/share/sdfs/mount.sdfs
 ls -lah /workspace/install-packages/deb/sbin/
 
